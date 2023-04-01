0ff2ee136e34bc70f55c2c71633ed995f7d0da00
谁书-ss <yushuishu@qq.com>
2023-04-02 00:21:35 +0800

    feat:helloworld

--- basics/01-HelloWorld.cpp
+++ basics/01-HelloWorld.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 
-/**
+/*
 1、新建项目
 2、c++语言
 3、选择空项目
 4、项目名称；类似于 Javaweb开发中的module模块
    解决方案：类似于 Javaweb开发创建的工程
 5、Visual Studio 创建工程之后，在左边窗口解决方案下的的项目包（module模块）下的源文件中创建 鼠标右键 -》添加 -》新建项 -》文件名称（java类文件名）
-6、编写 C++ 入门程序 main函数，相当于 java main函数
+6、编写 C++ 入门程序 main函数，程序入口，相当于 java main函数
+7、本地 Windows 调试器 ：运行自动打开cmd 黑窗口，输出 “hello C++”，并暂停程序结束，方便观察。
 
 */
 int main() {
