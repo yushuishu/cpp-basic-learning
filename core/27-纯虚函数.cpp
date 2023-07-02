8248d1408ea215f6f6c96d03df6d3a7d48ad1384
谁书-ss <yushuishu@qq.com>
2023-07-02 22:12:53 +0800

    feat:多态、虚函数、纯虚函数，虚析构、纯析构、io文本文件操作、io二进制操作

---
+++ core/27-纯虚函数.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+/**
+
+纯虚函数和抽象类：
+	基类的方法一般被子类实现函数的逻辑，所有基类的虚函数还可以简写为；virtual int getResult() = 0;
+	纯虚函数相当于java中的抽象方法
+
+*/
+
+// 基类
+class Base
+{
+public:
+	// 纯虚函数
+	// 只要有一个纯虚函数，这个类称为抽象类，抽象类特点：1、无法实例化对象，2、抽象类的子类，必须重写父类中的纯虚函数，否则也属于抽象类
+	virtual int getResult() = 0;
+	int num1;
+	int num2;
+};
+
+
+class Son : public Base {
+public:
+	int getResult() {
+
+	}
+};
+
+int main() {
+	//Base b;      // 抽象类无法实例化对象
+	//new Base;    // 抽象类无法实例化对象
+
+
+	//必须重写父类中的纯虚函数，否则也属于抽象类
+	Son s;
+}
---
+++ core/29-IO.cpp
@@ -0,0 +1,146 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+
+
+
+// 文本文件 写文件
+void test1() {
+	// 1、头文件 #include<fstream>
+
+	// 2、创建流对象
+	ofstream ofs;
+
+	// 3、指定打开方式，可以使用 | 配合使用，如：ios::out | ios:binary
+	//    ios::in	   读文件方式打开
+	//	  ios::out     写文件方式打开
+	//	  ios::ate     初始位置，文件末尾
+	//    ios::app     追加方式写文件
+	//    ios:trunc    如果文件存在先删除，再创建
+	//    ios:binary   二进制方式
+	ofs.open("test.txt", ios::out);
+
+	// 4、写内容
+	ofs << "姓名：张三" << endl;
+	ofs << "性别：男" << endl;
+	ofs << "年龄：20" << endl;
+
+	// 5、关闭文件
+	ofs.close();
+
+}
+
+// 文本文件  读文件
+void test2() {
+	// 1、头文件 #include<fstream>
+
+	// 2、创建流对象
+	ifstream ifs;
+
+	// 3、指定打开方式
+	ifs.open("test.txt", ios::in);
+
+	// 4、判断是否打开成功
+	if (!ifs.is_open()) {
+		cout << "打开文件失败" << endl;
+		return;
+	}
+
+	// 5、读数据
+	// 第一种
+	char buf[1024] = { 0 };
+	while (ifs >> buf)
+	{
+		cout << buf << endl;
+	}
+	// 第二种
+	/*char buf[1024] = { 0 };
+	while (ifs.getline(buf, sizeof(buf)))
+	{
+		cout << buf << endl;
+	}*/
+	// 第三种
+	/*string buf;
+	while (getline(ifs, buf))
+	{
+		cout << buf << endl;
+	}*/
+	// 第四种
+	/*char c;
+	while ((c = ifs.get()) != EOF)
+	{
+		cout << c;
+	}*/
+
+
+	// 6、关闭文件
+	ifs.close();
+
+}
+
+// 二进制数据对象
+class Person
+{
+public:
+	char m_name[64]; // 姓名
+	int m_age; // 年龄
+
+};
+
+// 二进制 写文件
+void test3() {
+	// 1、头文件 #include<fstream>
+	
+	// 2、创建流对象
+	ofstream ofs;
+
+	// 3、指定打开方式
+	ofs.open("person.txt", ios::out | ios::binary);
+
+	// 4、写文件数据
+	Person p = { "张三", 20 };
+	ofs.write((const char*)&p, sizeof(Person));
+
+	// 5、关闭文件
+	ofs.close();
+}
+
+// 二进制 读文件
+void test4() {
+	// 1、头文件 #include<fstream>
+
+	// 2、创建流对象
+	ifstream ifs;
+
+	// 3、指定打开方式
+	ifs.open("person.txt", ios::in | ios::binary);
+
+	// 4、判断是否打开成功
+	if (!ifs.is_open()) {
+		cout << "打开文件失败" << endl;
+		return;
+	}
+
+	// 5、读文件数据
+	Person p;
+	ifs.read((char*)&p, sizeof(Person));
+	cout << "姓名：" << p.m_name << " 年龄：" << p.m_age << endl;
+
+	// 6、关闭文件
+	ifs.close();
+}
+
+
+
+
+
+int main() {
+	// 文本文件
+	//test1();
+	//test2();
+
+	// 二进制
+	//test3();
+	test4();
+}
